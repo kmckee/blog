a4f7f879e6817a2c2ebb4c2275ca50560b2f4ca0
Kyle McKee <mckee.kyle@gmail.com>
2019-04-07 00:15:52 -0400

    Timeline fades in on page load for smoother ux.

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,34 +1,44 @@
-import React from 'react';
+import React from "react"
 
-import Layout from '../components/layout';
-import SkillDisplay from '../components/resume/skillDisplay';
-import EventsTimeline from '../components/resume/eventTimeline';
+import Layout from "../components/layout"
+import SkillDisplay from "../components/resume/skillDisplay"
+import EventsTimeline from "../components/resume/eventTimeline"
 import SEO from "../components/seo"
+import resumeStyles from "./resume.module.css"
 
-import resume from '../resumeData';
+import resume from "../resumeData"
 
-const ResumePage = ({location}) => {
-    return (
-        <Layout location={location} title={resume.name}>
-            <SEO
-            title="Kyle McKee - Agile Coach and Software Developer in Cleveland, OH"
-            keywords={[`agile coach`, `javascript`, `react`, `Kyle McKee`, `cleveland`, `consultant`, `C#`, `developer`]}
-            />
-            <header>
-                <p>{resume.summary}</p>
-            </header>
-            <main>
-                <section>
-                    <h2>Skills</h2>
-                    <SkillDisplay />
-                </section>
-                <section>
-                    <h2>Timeline</h2>
-                    <EventsTimeline events={resume.events} />
-                </section>
-            </main>
-        </Layout>
-    );
-};
+const ResumePage = ({ location }) => {
+  return (
+    <Layout location={location} title={resume.name}>
+      <SEO
+        title="Kyle McKee - Agile Coach and Software Developer in Cleveland, OH"
+        keywords={[
+          `agile coach`,
+          `javascript`,
+          `react`,
+          `Kyle McKee`,
+          `cleveland`,
+          `consultant`,
+          `C#`,
+          `developer`,
+        ]}
+      />
+      <header>
+        <p>{resume.summary}</p>
+      </header>
+      <main>
+        <section>
+          <h2>Skills</h2>
+          <SkillDisplay />
+        </section>
+        <section className={resumeStyles.timeline}>
+          <h2>Timeline</h2>
+          <EventsTimeline events={resume.events} />
+        </section>
+      </main>
+    </Layout>
+  )
+}
 
 export default ResumePage
